73faa8182ef57ab5af947457e3334e29eaf6363e
Ryansolar14 <76922192+Ryansolar14@users.noreply.github.com>
2023-11-19 20:51:34 -0500

    Update no interrupt

--- Main.ino
+++ Main.ino
@@ -10,7 +10,7 @@ const int interruptPin = 2;
 volatile boolean buttonAvailable = false;
 
 //Declare Global Variables
-char currentKey = '';
+char currentKey = "";
 int num = 0;
 String sequence = "";
 String number1 = "";
@@ -34,10 +34,10 @@ void setup() {
 
     Serial.println("Press a button and it will print here.");
 
-    pinMode(interruptPin, INPUT); // Qwiic Keypad holds INT pin HIGH @ 3.3V, then LOW when fired.
+    // pinMode(interruptPin, INPUT); // Qwiic Keypad holds INT pin HIGH @ 3.3V, then LOW when fired.
     // Note, this means we do not want INPUT_PULLUP.
 
-    attachInterrupt(digitalPinToInterrupt(interruptPin), intReadPrintButton, FALLING);
+    // attachInterrupt(digitalPinToInterrupt(interruptPin), intReadPrintButton, FALLING);
     // Note, INT on the Keypad will "fall" from HIGH to LOW when a new button has been pressed.
     // Also note, it will stay low while there are still button events on the stack.
     // This is useful if you want to "poll" the INT pin, rather than use a hardware interrupt.
@@ -46,23 +46,30 @@ void setup() {
 void loop() {
     if(buttonAvailable){
         keypad.updateFIFO();
-        currentKey = keypad.getButton();
+        char key = keypad.getButton();
+        if(key != 0){
+          currentKey = keypad.getButton();
+        }
+        Serial.println(char(currentKey));
         //If # save the number to a variable
         if(currentKey == '#'){
             num++; //Value to determine if it is the first or second number
             if(num == 1){
                 number1 = sequence;
+                Serial.println("Sequence 1" + number1);
             } else if(num == 2){
                 number2 = sequence;
+                Serial.println("Sequence 2" + number2);
             }
-        } else if(key == '*'){
+        } else if(currentKey == '*'){
             convertDecimalToBinary(number1.toInt(), add1);
             convertDecimalToBinary(number2.toInt(), add2);
-
         } else {
             sequence = sequence + currentKey;
         }
-        Serial.print(char(keypad.getButton()));
+        
+        delay(1000);
+        currentKey = 0;
         buttonAvailable = false;
     } 
 }
