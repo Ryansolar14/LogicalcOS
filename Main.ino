fee9f8ca812eca93558899d5d57855837e74216c
Ryansolar14 <76922192+Ryansolar14@users.noreply.github.com>
2023-12-03 18:43:04 -0500

    Update Overflow and Keypad Startup
    Update the overflow value since the circuit is being changed to 6-bits. Update the keypad initialization sequence to wait 500ms before entering infinite loop. Add commenting and delete unnecessary code.

--- Main.ino
+++ Main.ino
@@ -1,5 +1,7 @@
+//LogicalcOS - Arduino program to read a keypad and output the two numbers to be on two shift registers for a 6bit addition circuit.
+//Date: 12/03/2023
 //Code author: Ryan Busch
-//Credits: Sparkfun Library Example 5
+//Credits: Sparkfun Owiic Keypad Library Example 5
 
 #include <Wire.h>
 #include "SparkFun_Qwiic_Keypad_Arduino_Library.h"
@@ -34,6 +36,7 @@ int num1 = 0;
 int num2 = 0;
 
 void setup() {
+  //Set pin modes
   pinMode(statusPin, OUTPUT);
   pinMode(overflowPin, OUTPUT);
   pinMode(clockPin1, OUTPUT);
@@ -42,29 +45,33 @@ void setup() {
   pinMode(clockPin2, OUTPUT);
   pinMode(latchPin2, OUTPUT);
   pinMode(dataPin2, OUTPUT);
-
-  setRegister(0, clockPin1, latchPin1, dataPin1);
-  setRegister(0, clockPin2, latchPin2, dataPin2);
-
-  digitalWrite(statusPin, 0);
-  Serial.begin(9600);
-  Serial.println("Qwiic KeyPad Example 5 - InterruptRead");
-
-  if (keypad.begin() == false)
-  {
-    Serial.println("Keypad does not appear to be connected. Please check wiring. Freezing...");
-    while (1);
-  }
-
-  digitalWrite(statusPin, 1);
-  Serial.print("Initialized. Firmware Version: ");
-  Serial.println(keypad.getVersion());
-
-  Serial.println("Press a button and it will print here.");
-
   pinMode(interruptPin, INPUT);  
 
-  attachInterrupt(digitalPinToInterrupt(interruptPin), intReadPrintButton, FALLING);
+  //Set all shift register output to LOW so the display is initially zero
+  setRegister(0, clockPin1, latchPin1, dataPin1);
+  setRegister(0, clockPin2, latchPin2, dataPin2);
+ 
+  digitalWrite(statusPin, 0); //Only high if keypad is active
+
+  //Initialize Serial Monitor for debug
+  Serial.begin(9600);
+  Serial.println("*****LogicalcOS Debug Terminal*****");
+
+  //If keypad does not initialize, wait 500ms and try again. If it fails again, freeze the program.
+  if (keypad.begin() == false)
+  {
+    Serial.println("Keypad does not appear to be connected...");
+    delay(500);
+    if(keypad.begin() == false)
+    {
+      Serial.println("Keypad still does not appear to be connected. Freezing...");
+      while(1);
+    }
+  }
+
+  digitalWrite(statusPin, 1); //Display is active
+  
+  attachInterrupt(digitalPinToInterrupt(interruptPin), intReadPrintButton, FALLING); //Attach interrupt pin
 }
 
 void loop() {
@@ -72,24 +79,29 @@ void loop() {
     keypad.updateFIFO();
     char currentKey = keypad.getButton();
     Serial.println(char(currentKey));
-    //If # save the number to a variable
+    //If a number save it to a variable
     if (currentKey == '*') {
       num++;  //Value to determine if it is the first or second number
       if (num == 1) {
+        //Save current sequence to number 1
         number1 = sequence;
         num1 = number1.toInt();
         Serial.println("Sequence 1 " + number1);
         sequence = "";
       } else if (num == 2) {
+        //Save current sequence to number 2
         number2 = sequence;
         num2 = number2.toInt();
         Serial.println("Sequence 2 " + number2);
         sequence = "";
       } else {
+        //Error, this should never happen
         Serial.println("Exeption100!");
       }
+      //If key is #, check for an overflow and output the two numbers on the shift registers.
     } else if (currentKey == '#') {
-        if((num1 + num2) > 511){
+        //Check for overflow, if there is an overflow, set the overflow pin high.
+        if((num1 + num2) > 126){
           digitalWrite(overflowPin, HIGH);
         } else {
           digitalWrite(overflowPin, LOW);
@@ -97,15 +109,18 @@ void loop() {
           setRegister(num2, clockPin2, latchPin2, dataPin2);
         }
     } else {
+      //Update sequence
       sequence = sequence + currentKey;
     }
     buttonAvailable = false;
+    //Reset the counter that determines if the value is number 1 or number 2.
     if (num == 2) {
       num = 0;
     }
   }
 }
 
+//Interrupt function to read the button.
 void intReadPrintButton() {
   buttonAvailable = true;
 }
